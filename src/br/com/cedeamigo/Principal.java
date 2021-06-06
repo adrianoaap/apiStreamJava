95c3ef0e2102fc8cdf5f1e0dcf4cfeda714ad1f8
adrianoaap <ayrespalmeiraa@gmail.com>
2021-06-06 11:34:35 -0300

    git commit -m "criação da classe person"

--- src/br/com/cedeamigo/Principal.java
+++ src/br/com/cedeamigo/Principal.java
@@ -1,7 +1,20 @@
 package br.com.cedeamigo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Principal {
 
-    System.out.println("Hell World!")
+	public static void main(String[] args) {
 
+		List<Person> people = Arrays.asList(
+				new Person("Goku", 55), new Person("Ikki", 33), 
+				new Person("Naruto", 29), new Person("Pan", 5),
+				new Person("Seya", 29), new Person("Vegeta", 61),
+				new Person("Bills", 130), new Person("Hyoga", 30),
+				new Person("Itachi", 40), new Person("Jiraiya", 55));
+		
+		System.out.println(" " + people);
+
+	}
 }
